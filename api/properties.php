51cf63bc6c087a2f6f5752f2afd5c6d37dec7519
DuanShengpeng <duanshp@chineseall.com>
2015-07-09 22:48:02 +0800

    add properties

---
+++ api/properties.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: fixopen
+ * Date: 9/7/15
+ * Time: 22:29
+ */
+class properties extends Model
+{
+    public static function get($name)
+    {
+        //
+    }
+
+    public static function set($name, $value)
+    {
+        //
+    }
+}
